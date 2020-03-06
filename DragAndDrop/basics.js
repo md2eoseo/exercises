b0ec5380d23fdbd090a918d7b0b71e14af6d92c3
md2eoseo <md2eoseo@gmail.com>
2020-03-06 11:25:46 +0100

    DragAndDrop simple circle

--- DragAndDrop/basics.js
+++ DragAndDrop/basics.js
@@ -6,11 +6,30 @@ const HTML = {};
 async function start() {
   const response = await fetch("yy.svg");
   const mySvgData = await response.text();
-  document.querySelector(".svg").innerHTML = mySvgData;
+  document.querySelector("#svg").innerHTML = mySvgData;
   startManipulatingTheSvg();
 }
 
 function startManipulatingTheSvg() {
   console.log("startManipulatingTheSvg()");
-  HTML.svg = document.querySelector(".svg");
+  HTML.svg = document.querySelector("#svg");
+  HTML.circle = document.querySelector("#svg circle");
+
+  HTML.circle.addEventListener("mousedown", function() {
+    HTML.circle.addEventListener("mousemove", move);
+  });
+
+  window.addEventListener("mouseup", function() {
+    HTML.circle.removeEventListener("mousemove", move);
+  });
+}
+
+function move(e) {
+  e.target.setAttribute("cx", (e.pageX / window.innerWidth) * 512);
+  e.target.setAttribute(
+    "cy",
+    (e.pageY / window.innerHeight) *
+      512 *
+      (window.innerHeight / window.innerWidth)
+  );
 }
