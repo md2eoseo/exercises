2199338d2c05cc0973b404e27ef8f9ca9b142692
md2eoseo <md2eoseo@gmail.com>
2020-03-07 16:14:42 +0100

    DragAndDrop excercise complete

--- DragAndDrop/basics.js
+++ DragAndDrop/basics.js
@@ -4,32 +4,45 @@ window.addEventListener("DOMContentLoaded", start);
 const HTML = {};
 
 async function start() {
-  const response = await fetch("yy.svg");
-  const mySvgData = await response.text();
-  document.querySelector("#svg").innerHTML = mySvgData;
+  // const response = await fetch("yy.svg");
+  // const mySvgData = await response.text();
+  // document.querySelector("#svg").innerHTML = mySvgData;
   startManipulatingTheSvg();
 }
 
 function startManipulatingTheSvg() {
   console.log("startManipulatingTheSvg()");
-  HTML.svg = document.querySelector("#svg");
-  HTML.circle = document.querySelector("#svg circle");
+  // HTML.circle = document.querySelector("#svg circle");
+  HTML.img = document.querySelector("#svg");
+  HTML.image = document.querySelector("image");
 
-  HTML.circle.addEventListener("mousedown", function() {
-    HTML.circle.addEventListener("mousemove", move);
+  HTML.img.addEventListener("mousedown", function() {
+    console.log("mousedown");
+    HTML.img.addEventListener("mousemove", move);
   });
 
   window.addEventListener("mouseup", function() {
-    HTML.circle.removeEventListener("mousemove", move);
+    console.log("mouseup");
+    HTML.img.removeEventListener("mousemove", move);
   });
+
+  // HTML.circle.addEventListener("mousedown", function() {
+  //   HTML.circle.addEventListener("mousemove", move);
+  // });
+
+  // window.addEventListener("mouseup", function() {
+  //   HTML.circle.removeEventListener("mousemove", move);
+  // });
 }
 
 function move(e) {
-  e.target.setAttribute("cx", (e.pageX / window.innerWidth) * 512);
-  e.target.setAttribute(
-    "cy",
+  e.preventDefault();
+
+  HTML.image.setAttribute("x", (e.pageX / window.innerWidth) * 1024);
+  HTML.image.setAttribute(
+    "y",
     (e.pageY / window.innerHeight) *
-      512 *
+      1024 *
       (window.innerHeight / window.innerWidth)
   );
 }
