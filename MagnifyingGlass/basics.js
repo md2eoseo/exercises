b0621484ed62afbb2899c0ba95c423390021fba6
md2eoseo <md2eoseo@gmail.com>
2020-03-08 17:40:25 +0100

    MagnifyingGlass can move clipPath circle in zoomed map svg

--- MagnifyingGlass/basics.js
+++ MagnifyingGlass/basics.js
@@ -7,9 +7,37 @@ async function start() {
   console.log("basics.js loaded");
   const response = await fetch("map.svg");
   const mySvgData = await response.text();
-  document.querySelector("section#original").innerHTML = mySvgData;
+  // document.querySelector("section#original").innerHTML = mySvgData;
   document.querySelector("section#zoomed").innerHTML = mySvgData;
   startManipulatingTheSvg();
 }
 
-function startManipulatingTheSvg() {}
+function startManipulatingTheSvg() {
+  // document.querySelectorAll("clipPath").forEach((ele, i) => {
+  //   if (i === 0) ele.classList.add("clip_original");
+  //   if (i === 1) ele.classList.add("clip_zoomed");
+  // });
+  HTML.glass = document.querySelector("#glass");
+  console.log(HTML.glass);
+
+  window.addEventListener("mousedown", function(e) {
+    console.log("mousedown");
+    move(e);
+    window.addEventListener("mousemove", move);
+  });
+  window.addEventListener("mouseup", function(e) {
+    console.log("mouseup");
+    move(e);
+    window.removeEventListener("mousemove", move);
+  });
+}
+
+function move(e) {
+  HTML.glass.setAttribute("cx", (e.pageX / window.innerWidth) * 950);
+  HTML.glass.setAttribute(
+    "cy",
+    (e.pageY / window.innerHeight) *
+      950 *
+      (window.innerHeight / window.innerWidth)
+  );
+}
