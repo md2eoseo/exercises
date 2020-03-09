74cae5c241d9b675794260fdd7a8feac2819730f
md2eoseo <md2eoseo@gmail.com>
2020-03-09 20:14:09 +0100

    timeline loaded JSON file

--- timeline/timeline.js
+++ timeline/timeline.js
@@ -6,4 +6,26 @@ async function start() {
   const response = await fetch("final.svg");
   const timelineSVG = await response.text();
   document.querySelector("#svg").innerHTML = timelineSVG;
+  loadJSON();
 }
+
+async function loadJSON() {
+  const response = await fetch("potterfilms.json");
+  const jsonData = await response.json();
+  console.log(jsonData);
+}
+
+// function createBalloon() {
+//   // create a single balloon, using the #yellow_balloon
+//   const use = document.createElementNS("http://www.w3.org/2000/svg", "use");
+//   // give it a random x and y
+//   use.setAttribute("href", "#yellow_balloon");
+//   use.setAttribute("x", `${Math.random() * 5000}`);
+//   use.setAttribute("y", `${Math.random() * 10000}`);
+
+//   console.log(use);
+//   // maybe also add a bit of animation ...
+//   // before adding it to the #balloons
+//   balloons.append(use);
+//   // use.appendChild(balloons);
+// }
