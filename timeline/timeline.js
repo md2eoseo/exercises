7be350ad71e814c1df2f545822f333e6c48ad1af
md2eoseo <md2eoseo@gmail.com>
2020-03-09 20:14:31 +0100

    timeline loaded JSON file

--- timeline/timeline.js
+++ timeline/timeline.js
@@ -14,18 +14,3 @@ async function loadJSON() {
   const jsonData = await response.json();
   console.log(jsonData);
 }
-
-// function createBalloon() {
-//   // create a single balloon, using the #yellow_balloon
-//   const use = document.createElementNS("http://www.w3.org/2000/svg", "use");
-//   // give it a random x and y
-//   use.setAttribute("href", "#yellow_balloon");
-//   use.setAttribute("x", `${Math.random() * 5000}`);
-//   use.setAttribute("y", `${Math.random() * 10000}`);
-
-//   console.log(use);
-//   // maybe also add a bit of animation ...
-//   // before adding it to the #balloons
-//   balloons.append(use);
-//   // use.appendChild(balloons);
-// }
