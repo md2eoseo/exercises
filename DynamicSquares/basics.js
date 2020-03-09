9416ef637dcfaed4597611d4079b39ecbf79ade2
md2eoseo <md2eoseo@gmail.com>
2020-03-09 10:59:55 +0100

    DynamicSquares changed squares to svg circles

--- DynamicSquares/basics.js
+++ DynamicSquares/basics.js
@@ -6,16 +6,30 @@ const HTML = {};
 function start() {
   console.log("basics.js loaded");
 
-  const body = document.querySelector("body");
+  // const body = document.querySelector("body");
+  const circles = document.querySelector("#circles");
   let num = 40;
 
   while (num--) {
-    const div = document.createElement("div");
-    div.classList.add("square");
-    div.style.top = `${Math.random() * 1024}px`;
-    div.style.left = `${Math.random() * 1024}px`;
-    div.style.backgroundColor = `rgb(${Math.random() * 256}, ${Math.random() *
-      256}, ${Math.random() * 256}`;
-    body.appendChild(div);
+    //   const div = document.createElement("div");
+    //   div.classList.add("square");
+    //   div.style.top = `${Math.random() * 1024}px`;
+    //   div.style.left = `${Math.random() * 1024}px`;
+    //   div.style.backgroundColor = `rgb(${Math.random() * 256}, ${Math.random() *
+    //     256}, ${Math.random() * 256}`;
+    //   body.appendChild(div);
+    const circle = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "circle"
+    );
+    circle.setAttribute("cx", Math.random() * 640);
+    circle.setAttribute("cy", Math.random() * 640);
+    circle.setAttribute(
+      "fill",
+      `rgb(${Math.random() * 256}, ${Math.random() * 256}, ${Math.random() *
+        256}`
+    );
+    circle.setAttribute("r", 50);
+    circles.appendChild(circle);
   }
 }
