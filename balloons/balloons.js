7f9de11b960e6a243c4a3fcae0bdcc8372f561a6
md2eoseo <md2eoseo@gmail.com>
2020-03-09 13:19:24 +0100

    balloons showing random balloons (what's the difference between appendChild and append)

--- balloons/balloons.js
+++ balloons/balloons.js
@@ -2,9 +2,16 @@
 
 window.addEventListener("DOMContentLoaded", start);
 
-function start() {
+const balloons = document.querySelector("#balloons");
+console.log(balloons);
+
+async function start() {
   // load the SVG into #balloon
+  const response = await fetch("Yellow_toy_balloon.svg");
+  const balloonSVG = await response.text();
+  document.querySelector("#balloon").innerHTML = balloonSVG;
   // then call createBalloons
+  createBalloons();
 }
 
 function createBalloons() {
@@ -15,7 +22,15 @@ function createBalloons() {
 
 function createBalloon() {
   // create a single balloon, using the #yellow_balloon
+  const use = document.createElementNS("http://www.w3.org/2000/svg", "use");
   // give it a random x and y
+  use.setAttribute("href", "#yellow_balloon");
+  use.setAttribute("x", `${Math.random() * 5000}`);
+  use.setAttribute("y", `${Math.random() * 10000}`);
+
+  console.log(use);
   // maybe also add a bit of animation ...
   // before adding it to the #balloons
+  balloons.append(use);
+  // use.appendChild(balloons);
 }
