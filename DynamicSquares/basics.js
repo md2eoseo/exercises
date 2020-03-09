8802b1e6e03c2f0b95981be5d0ab5486302f5cc2
md2eoseo <md2eoseo@gmail.com>
2020-03-09 11:27:14 +0100

    DynamicSquares using kind of template in svg tag and clone it

--- DynamicSquares/basics.js
+++ DynamicSquares/basics.js
@@ -8,6 +8,7 @@ function start() {
 
   // const body = document.querySelector("body");
   const circles = document.querySelector("#circles");
+  const clone = document.querySelector("#template");
   let num = 40;
 
   while (num--) {
@@ -18,18 +19,27 @@ function start() {
     //   div.style.backgroundColor = `rgb(${Math.random() * 256}, ${Math.random() *
     //     256}, ${Math.random() * 256}`;
     //   body.appendChild(div);
-    const circle = document.createElementNS(
-      "http://www.w3.org/2000/svg",
-      "circle"
-    );
-    circle.setAttribute("cx", Math.random() * 640);
-    circle.setAttribute("cy", Math.random() * 640);
-    circle.setAttribute(
-      "fill",
-      `rgb(${Math.random() * 256}, ${Math.random() * 256}, ${Math.random() *
-        256}`
-    );
-    circle.setAttribute("r", 50);
+
+    // const circle = document.createElementNS(
+    //   "http://www.w3.org/2000/svg",
+    //   "circle"
+    // );
+    // circle.setAttribute("cx", Math.random() * 640);
+    // circle.setAttribute("cy", Math.random() * 640);
+    // circle.setAttribute(
+    //   "fill",
+    //   `rgb(${Math.random() * 256}, ${Math.random() * 256}, ${Math.random() *
+    //     256}`
+    // );
+    // circle.setAttribute("r", 50);
+
+    const circle = clone.querySelector("circle").cloneNode(true);
+
+    circle.style.cx = Math.random() * 640;
+    circle.style.cy = Math.random() * 640;
+    circle.style.fill = `rgb(${Math.random() * 256}, ${Math.random() *
+      256}, ${Math.random() * 256}`;
+    // circle.style.r = 50;
     circles.appendChild(circle);
   }
 }
