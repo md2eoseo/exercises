e751d94861d298d8b249e0bc7e5fd70ae4f17eef
md2eoseo <md2eoseo@gmail.com>
2020-03-10 12:02:05 +0100

    Scroll added scroll status bar

--- Scroll/basics.js
+++ Scroll/basics.js
@@ -5,4 +5,15 @@ const HTML = {};
 
 function start() {
   console.log("basics.js loaded");
+  HTML.cnt = document.querySelector(".the_container");
+  HTML.cnt.addEventListener("scroll", scrolling);
+  scrolling();
+}
+
+function scrolling() {
+  const ratio =
+    HTML.cnt.scrollTop / (HTML.cnt.scrollHeight - HTML.cnt.clientHeight);
+  document
+    .querySelector("#scrollinfo")
+    .style.setProperty("--scroll-ratio", ratio);
 }
